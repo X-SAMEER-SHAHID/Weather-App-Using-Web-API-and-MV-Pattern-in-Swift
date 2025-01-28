bc6ecd73b61be906cda601dd0ab35bd80b14497f
sameer-shahid <sameershahid5911@gmail.com>
2025-01-28 13:03:58 +0500

    Initial Commit

---
+++ Weather App Using Web API and MV Pattern in Swift/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Weather App Using Web API and MV Pattern in Swift
+//
+//  Created by Sameer,s Macbook on 1/28/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Weather App Using Web API and MV Pattern in Swift/Weather_App_Using_Web_API_and_MV_Pattern_in_SwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  Weather_App_Using_Web_API_and_MV_Pattern_in_SwiftApp.swift
+//  Weather App Using Web API and MV Pattern in Swift
+//
+//  Created by Sameer,s Macbook on 1/28/25.
+//
+
+import SwiftUI
+
+@main
+struct Weather_App_Using_Web_API_and_MV_Pattern_in_SwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
